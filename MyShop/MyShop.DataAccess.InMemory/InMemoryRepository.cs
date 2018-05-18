6f0cfd6ea85f07f6e62699d1708c65d7f715bc15
ecejmsr <ecejmsr>
2018-05-18 17:41:49 -0500

    Added Generic without base class

---
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class InMemoryRepository<T>
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string className;
+        
+        public InMemoryRepository()
+        {
+            className = typeof(T).Name;
+            items = cache[className] as List<T>;
+            if(items== null)
+            {
+                items = new List<T>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache[className] = items;
+        }
+
+        public void Insert(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Update(T item)
+        {
+            T itemToUpdate = items.Find(i => i.ID == item.ID);
+
+            if (itemToUpdate != null)
+            {
+                itemToUpdate = item;
+            }
+            else
+            {
+                throw new Exception(className+ " Is Not Found");
+            }
+        }
+
+        public T Find(string ID)
+        {
+            T itemToFind = items.Find(i => i.ID == ID);
+
+            if(itemToFind != null)
+            {
+                return itemToFind;
+            }
+            else
+            {
+                throw new Exception(className + " Is Not Found");
+            }
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(string ID)
+        {
+            T itemToDelete = items.Find(i => i.ID == ID);
+
+            if(itemToDelete != null)
+            {
+                items.Remove(itemToDelete);
+            }
+            else
+            {
+                throw new Exception(className + " Is Not Found");
+            }
+        }
+    }
+}
