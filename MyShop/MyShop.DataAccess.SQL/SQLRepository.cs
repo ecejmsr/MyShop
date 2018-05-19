2c3de967dd2d5da7bc6a32ea17ae6a5748740d0e
ecejmsr <ecejmsr>
2018-05-19 17:27:26 -0500

    Added SQL methods to work with table, updated DI containters to work with the SQL Repository.

---
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,60 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string ID)
+        {
+            var item = Find(ID);
+            if(context.Entry(item).State == EntityState.Modified)
+            {
+                dbSet.Attach(item);
+            }
+
+            dbSet.Remove(item);
+        }
+
+        public T Find(string ID)
+        {
+            return dbSet.Find(ID);
+        }
+
+        public void Insert(T item)
+        {
+            dbSet.Add(item);
+        }
+
+        public void Update(T item)
+        {
+            dbSet.Attach(item);
+            context.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
